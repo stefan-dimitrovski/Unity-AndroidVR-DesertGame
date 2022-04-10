a33e9f719dbffc4aef963d391bd3b1c5a50c5428
stefan <stefan.dimitrovski_19@hotmail.com>
2022-04-11 00:43:39 +0200

    init

---
+++ VR_Project/Assets/LB3D/Bus/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    
+    public GameObject asset;
+    
+    public Slider rotate;
+    public Slider zoom;
+    public Camera cam;
+
+    public Toggle doorControl;
+
+    private void Update()
+    {
+        asset.transform.eulerAngles = new Vector3(0, rotate.value * 360, 0);
+        cam.orthographicSize = zoom.value;
+    }
+}
